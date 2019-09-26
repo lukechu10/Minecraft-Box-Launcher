1de408f2d5cee37c5674d0dcbdb1783223ec87cc
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-09-25 19:56:24 -0700

    Modified authentication module

--- src/ts/authentication.ts
+++ src/ts/authentication.ts
@@ -1,7 +1,15 @@
+import ElectronStore = require('electron-store');
 import { Auth } from '@xmcl/minecraft-launcher-core';
 
-export async function login(username: string, password: string): Promise<string> {
+const instancesData = new ElectronStore();
+
+/**
+ * Sends a request Yggdrasil auth server and stores the returned data
+ * @param username Minecraft email
+ * @param password Minecraft password
+ */
+export async function login(username: string, password: string): Promise<Auth> {
     const authFromMojang: Auth = await Auth.Yggdrasil.login({ username, password }); // official login
-    const accessToken: string = authFromMojang.accessToken;
-    return accessToken;
+    // const accessToken: string = authFromMojang.accessToken;
+    return authFromMojang;
 }
