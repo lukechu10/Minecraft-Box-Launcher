3c902e04c930237119133ab388de9c763261b4f1
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-09-29 12:17:26 -0700

    Fixed all eslint errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -21,13 +21,13 @@ function createWindow() {
 	mainWindow.setMenu(null);
 
 	// and load the index.html of the app.
-	mainWindow.loadFile('./views/index.html');
+	mainWindow.loadFile(path.join("./views/", "index.html"));
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	mainWindow.webContents.openDevTools();
 
 	// Emitted when the window is closed.
-	mainWindow.on('closed', function() {
+	mainWindow.on("closed", function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -38,16 +38,16 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on("window-all-closed", function() {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', function() {
+app.on("activate", function() {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) createWindow();
