6a8e1f9127d50782527a8e51f804328e0c5e7a00
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-10-13 17:36:32 -0700

    chore: ESLint Fix

--- src/ts/store/InstanceStore.ts
+++ src/ts/store/InstanceStore.ts
@@ -11,39 +11,39 @@ export default class InstanceStore extends Store {
 			},
 			schema: {
 				instances: {
-                    type: "array",
-                    // TODO: Improve schema to check for InstanceSave structure
+					type: "array",
+					// TODO: Improve schema to check for InstanceSave structure
 				}
 			}
 		});
-    }
-    /**
+	}
+	/**
      * Get list of instances
      */
-    get instances(): InstanceSave[] {
-        return this.get("instances") as InstanceSave[];
-    }
-    /**
+	get instances(): InstanceSave[] {
+		return this.get("instances") as InstanceSave[];
+	}
+	/**
      * Add a new instance to the store
      * @param item Instance to be added
      */
-    addInstance(item: InstanceSave): void {
-        // TODO: check for name already exists
-        const result = this.instances.find(obj => obj.name == item.name);
-        if (result !== undefined) throw Error("An instance with this name already exists!");
-        else this.set("instances", this.get("instances").concat(item));
-    }
-    /**
+	addInstance(item: InstanceSave): void {
+		// TODO: check for name already exists
+		const result = this.instances.find(obj => obj.name == item.name);
+		if (result !== undefined) throw Error("An instance with this name already exists!");
+		else this.set("instances", this.get("instances").concat(item));
+	}
+	/**
      * Deletes an instance by name
      * @throws {Error} if no instance is found
      */
-    deleteInstance(name: string) {
-        // FIXME: fix .splice
-        const index: number = this.instances.findIndex(obj => obj.name == name);
-        console.log(index)
-        if (index == -1) throw Error("An instance with this name does not exist");
-        const temp = Array.from(this.instances);
-        console.log(temp.splice(index))
-        this.set("instances", temp);
-    }
+	deleteInstance(name: string) {
+		// FIXME: fix .splice
+		const index: number = this.instances.findIndex(obj => obj.name == name);
+		console.log(index);
+		if (index == -1) throw Error("An instance with this name does not exist");
+		const temp = Array.from(this.instances);
+		console.log(temp.splice(index));
+		this.set("instances", temp);
+	}
 }
