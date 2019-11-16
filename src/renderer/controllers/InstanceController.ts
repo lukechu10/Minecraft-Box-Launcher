37a6628fe00b064bf6dbe2b0b665d318b3392724
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-11-15 21:55:04 -0800

    fix: incomplete options for launch()

--- src/renderer/controllers/InstanceController.ts
+++ src/renderer/controllers/InstanceController.ts
@@ -1,9 +1,13 @@
 // TODO: Rename all imports from store as AppStore and export namespace
 import * as AppStore from "../../universal/store";
 import InstanceSave from "../instance/InstanceSave";
+import * as consoleUtils from "../../universal/consoleUtils";
+
 import path from "path";
 import child_process from "child_process";
+
 import { Launcher } from "@xmcl/launch";
+import { Auth } from "@xmcl/auth";
 
 import { remote, ipcRenderer } from "electron";
 const app = remote.app;
@@ -19,10 +23,11 @@ export namespace InstanceController {
 	 */
 	export var MinecraftSavePathBase: string = path.join(app.getPath("userData"), "./instances/");
 
+	// FIXME: Function not working correctly on windows
 	/**
 	 * Find where java is installed on local machine 
 	 */
-	function findJava(): string {
+	export function findJava(): string {
 		var spawn = child_process.spawnSync("which", ["java"]).stdout;
 		return spawn;
 	}
@@ -37,11 +42,17 @@ export namespace InstanceController {
 			throw "An instance with this name does not exist";
 		}
 		else {
-			const proc = Launcher.launch({
+			const opts: Launcher.Option & Launcher.PrecheckService = {
 				gamePath: MinecraftSavePathBase,
-				javaPath: findJava(),
 				version: instance.id,
-			});
+				javaPath: "java", // TODO: Change to executable path if java is not in %PATH%
+				launcherName: "Minecraft Box Launcher",
+				user: AppStore.auth.store as Auth,
+				auth: AppStore.auth.store as Auth
+			}
+			consoleUtils.debug(`Launching instance ${name} with options: `, opts);
+			// span game
+			const proc = Launcher.launch(opts);
 		}
 	}
 }
