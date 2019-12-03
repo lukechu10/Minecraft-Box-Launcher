4ed8e305dea54ee4def7177e2e24ee159fe1755b
Luke Chu <lukechu10@gmail.com>
2019-12-03 20:59:48 +0000

    feat: add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	mode: "development",
+	devtool: "inline-source-map",
+	entry: "./src/renderer/StartupTasks.ts",
+	output: {
+		filename: "./dist/renderer.bundle.js",
+		libraryTarget: "window"
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	},
+	target: "electron-renderer"
+};
