a83be9d0d18f3be62401fceae415793033960fe3
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-12-13 20:27:20 -0800

    test: limit webpack loaders to improve build performance

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,16 @@ module.exports = {
 	module: {
 		rules: [
 			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-			{ test: /\.tsx?$/, loader: "ts-loader" },
-			{ test: /\.pug/, loader: "pug-loader"}
+			{
+				test: /\.tsx?$/,
+				include: path.resolve(__dirname, "src"),
+				loader: "ts-loader"
+			},
+			{
+				test: /\.pug/,
+				include: path.resolve(__dirname, "src", "renderer"),
+				loader: "pug-loader"
+			}
 		]
 	},
 	target: "electron-renderer",
