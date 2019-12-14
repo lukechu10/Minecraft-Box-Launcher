758f28d85381ef5c3dc0f7050fa2b25b6a13e335
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-12-14 12:28:25 -0800

    fix: remove useless imports and ipc channels from main process

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,15 +1,10 @@
 import "v8-compile-cache";
 
 // Modules to control application life and create native browser window
-import { app, ipcMain, Menu, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, Menu, BrowserWindow } from "electron";
 
 import debug from "electron-debug";
 
-import * as consoleUtils from "../universal/consoleUtils";
-import Window, { WindowConstructorOptions } from "./Window";
-import { ApplicationStore } from "../universal/store";
-import { InstanceSave } from "../universal/store/InstanceSave";
 import { MainWindow } from "./MainWindow";
 
 debug({
@@ -17,29 +12,6 @@ debug({
 });
 
 const WindowList: Map<string, BrowserWindow | null> = new Map();
-
-class WindowOptsList {
-	public main: WindowConstructorOptions = {
-		type: "file",
-		path: path.join(__dirname, "../../views/", "instances.html"),
-		width: 800,
-		height: 600,
-		title: "Minecraft Box",
-		webPreferences: {
-			nodeIntegration: true,
-			sandbox: false
-		}
-	};
-	public instanceOptions: WindowConstructorOptions = {
-		type: "file",
-		path: path.join(__dirname, "../../views/", "instanceOptions.html"),
-		title: "Options",
-		webPreferences: {
-			nodeIntegration: true
-		}
-	};
-}
-const windowsOpts: WindowOptsList = new WindowOptsList();
 
 if (!process.argv.includes("--dev")) // show application menu only if flag --dev is passed as 3rd argument
 	Menu.setApplicationMenu(null); // only show menu in dev
@@ -78,65 +50,3 @@ app.on("activate", () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-// windows
-ipcMain.on("showWindow-instanceOptions", (event: Electron.IpcMainEvent, ...args: any[]) => {
-	/*
-	NOTE: instance option windows are represented as `instanceOption-${instance name}` in WindowList: Map<string, BrowserWindow | null>
-	*/
-	try {
-		if (args.length === 0)
-			throw RangeError("args must at least have one element");
-		else if (typeof args[0] !== "string")
-			throw TypeError("expected argument args[0] to be type string");
-		else {
-			const instanceName: string = args[0];
-			// check if instance exists
-			const findResult: InstanceSave | undefined = ApplicationStore.instances.findFromName(instanceName);
-			if (findResult === undefined) {
-				throw Error(`an instance named ${instanceName} does not exist`);
-			}
-			else {
-				// check if window has already been created
-				if (!WindowList.has(`instanceList-${instanceName}`) || WindowList.get(`instanceList-${instanceName}`) === null) {
-					// create window and push to array
-					consoleUtils.debug(`Creating options window for instance ${instanceName}`);
-					// get opts
-					let opts = windowsOpts.instanceOptions;
-					// set title and queries
-					opts.title = `${instanceName} | Options`;
-					// create window
-					let newWindow: Window | null = new Window(opts);
-					(newWindow as any).instanceName = instanceName; // FIXME: remove work around as this breaks BrowserWindow type
-					(newWindow as any).startPage = args[1] || "general";
-					newWindow.on("closed", () => {
-						WindowList.set(`instanceList-${instanceName}`, null);
-					});
-					// create / recreate window
-					WindowList.set(`instanceList-${instanceName}`, newWindow);
-				}
-				else {
-					// window already created: send switch page request with ipc
-					WindowList.get(`instanceList-${instanceName}`)?.webContents.send("switchPage", args[1]);
-				}
-				// show window
-				consoleUtils.debug(`Showing options window for instance ${instanceName}`);
-				WindowList.get(`instanceList-${instanceName}`)?.show();
-				event.returnValue = { success: true, message: "window successfully created" };
-			}
-		}
-	}
-	catch (err) {
-		// return error
-		console.error(err);
-		event.returnValue = { success: false, message: err.message };
-	}
-});
-
-
-// change to instance list from different window
-ipcMain.on("new-instance", (event: Electron.IpcMainEvent) => {
-	consoleUtils.debug("Updating instance list");
-	if (WindowList.get("main") !== null)
-		WindowList.get("main")?.webContents.send("update-instance-list");
-	event.returnValue = "success";
-});
