aced506ac553ca73cd55a3a9d10df7284c874709
Luke Chu <lukechu10@gmail.com>
2020-01-12 20:53:54 +0000

    refactor: change InstanceData to interface

--- src/renderer/store/InstanceData.ts
+++ src/renderer/store/InstanceData.ts
@@ -3,50 +3,35 @@ import { Installer } from "@xmcl/installer";
 /**
  * InstanceSave without methods
  */
-export class InstanceData {
+export interface InstanceData {
 	/**
 		 * Name of instance
 		 */
-	public name: string;
+	name: string;
 	/**
      * Instance version
      */
-	public id: string;
+	id: string;
 	/**
      * Mojang release or snapshot (vanilla only)
      */
-	public type: string;
+	type: string;
 	/**
      * Type of client
      */
-	public clientType: "vanilla" | "forge";
+	clientType: "vanilla" | "forge";
 	/**
 	 * Format: `new Date().toISOString()` or `"never"` if never played
 	 */
-	public lastPlayed: string | "never";
+	lastPlayed: string | "never";
 	/**
 	 * Date version was released
 	 */
-	public releaseTime: string;
-	public url: string;
+	releaseTime: string;
+	url: string;
 	/**
 	 * Version binaires are completely installed
 	 */
-	public installed: boolean = false;
-	public time: string;
-
-
-	/**
-	 * Create a save from VersionMeta
-	 */
-	public constructor(name: string, data: Installer.VersionMeta) {
-		this.name = name;
-		this.id = data.id;
-		this.type = data.type;
-		this.releaseTime = data.releaseTime;
-		this.lastPlayed = "never"; // now
-		this.clientType = "vanilla";
-		this.url = data.url;
-		this.time = data.time;
-	}
+	installed: boolean;
+	time: string;
 }
--- src/renderer/store/InstanceSave.ts
+++ src/renderer/store/InstanceSave.ts
@@ -16,7 +16,51 @@ else {
 	app = mainApp;
 }
 
-export class InstanceSave extends InstanceData implements Installer.VersionMeta {
+export class InstanceSave implements InstanceData, Installer.VersionMeta {
+	/**
+		 * Name of instance
+		 */
+	public name: string;
+	/**
+     * Instance version
+     */
+	public id: string;
+	/**
+     * Mojang release or snapshot (vanilla only)
+     */
+	public type: string;
+	/**
+     * Type of client
+     */
+	public clientType: "vanilla" | "forge";
+	/**
+	 * Format: `new Date().toISOString()` or `"never"` if never played
+	 */
+	public lastPlayed: string | "never";
+	/**
+	 * Date version was released
+	 */
+	public releaseTime: string;
+	public url: string;
+	/**
+	 * Version binaires are completely installed
+	 */
+	public installed: boolean = false;
+	public time: string;
+	/**
+	 * Create a save from VersionMeta
+	 */
+	public constructor(name: string, data: Installer.VersionMeta) {
+		this.name = name;
+		this.id = data.id;
+		this.type = data.type;
+		this.releaseTime = data.releaseTime;
+		this.lastPlayed = "never"; // now
+		this.clientType = "vanilla";
+		this.url = data.url;
+		this.time = data.time;
+	}
+
 	/**
 	 * Install this version
 	 */
