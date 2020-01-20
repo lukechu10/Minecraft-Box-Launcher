f915660bc05093d42310a57b8c4008a28185ba37
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-01-20 10:41:20 -0800

    feat: add toast for uncaught exceptions

--- src/renderer/StartupTasks.ts
+++ src/renderer/StartupTasks.ts
@@ -4,6 +4,8 @@ import { VersionsController } from "./controllers/VersionsController";
 import { InstanceController } from "./controllers/InstanceController";
 import * as Render from "./Render";
 
+import { shell } from "electron";
+
 // turbolinks events
 document.addEventListener("turbolinks:load", () => {
 	// update login status
@@ -11,6 +13,27 @@ document.addEventListener("turbolinks:load", () => {
 		Render.updateLoginStatus("logout");
 	}
 	else Render.updateLoginStatus("login");
+});
+
+window.addEventListener("unhandledrejection", (event) => {
+	// show toast with error message
+	// @ts-ignore
+	$("body").toast({
+		class: "error",
+		message: `<strong>An unexpected error occured</strong>:<br>${event.reason}`,
+		displayTime: 0,
+		classActions: "top attached",
+		actions: [{
+			text: "Report issue",
+			class: "yellow",
+			click: () => {
+				shell.openExternal("https://github.com/lukechu10/Minecraft-Box-Launcher/issues/new/choose");
+			}
+		}, {
+			text: "Ignore",
+			class: "orange"
+		}]
+	});
 });
 
 // startup tasks (on application start)
