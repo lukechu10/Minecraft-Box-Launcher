c8acfa6dd5b0a23f0c17cbcbbe66aad39cbb9465
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-02-02 16:29:20 -0800

    feat: make app single instance application
    It is no longer possible to open two instances of the app side by side.

--- src/main/main.ts
+++ src/main/main.ts
@@ -29,10 +29,24 @@ function createWindow(): void {
 	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+	app.quit();
+}
+else {
+	app.on("second-instance", () => {
+		// Someone tried to run a second instance, we should focus our window.
+		if (WindowList.get("main")?.isMinimized()) WindowList.get("main")?.restore();
+		WindowList.get("main")?.focus();
+	});
+
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.on("ready", () => {
+		createWindow();
+	});
+}
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
