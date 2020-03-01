c854a37e98860a0e5408964356850ab98cdfaab4
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-01 14:37:50 -0800

    docs: typo in JSDocs

--- src/renderer/controllers/LaunchController.ts
+++ src/renderer/controllers/LaunchController.ts
@@ -7,11 +7,10 @@ import { InstanceData } from "../store/InstanceData";
 import { InstanceController } from "./InstanceController";
 import child_process, { ChildProcess } from "child_process";
 
-import { Launcher } from "@xmcl/launch";
+import { launch, LaunchOption, LaunchPrecheck } from "@xmcl/core";
 import { ProfileService } from "@xmcl/profile-service";
 import { Auth } from "@xmcl/auth";
 import { MinecraftFolder } from "@xmcl/util";
-import { ResolvedVersion, Version } from "@xmcl/version";
 import * as JavaInstall from "./JavaInstall";
 
 export namespace LaunchController {
@@ -20,7 +19,7 @@ export namespace LaunchController {
 		 * @param instance data associated with instance (does not have to be in `InstanceStore`)
 		 * @throws if an instance with `name` does not exist
 		 * @throws if user is not logged in
-		 * @returns a chil process that was spawned or `null` if fail
+		 * @returns a child process that was spawned or `null` if fail
 		 */
 	export async function launch(instance: InstanceData): Promise<ChildProcess> {
 		let javaPath = ApplicationStore.GlobalSettings.store.java.externalJavaPath;
@@ -35,7 +34,7 @@ export namespace LaunchController {
 				javaPath = info[0].path;
 			}
 		}
-		const options: Launcher.Option & Launcher.PrecheckService = {
+		const options: LaunchOption & LaunchPrecheck = {
 			gamePath: InstanceController.MinecraftSavePath(instance.name),
 			resourcePath: InstanceController.MinecraftGamePath,
 			version: await Version.parse(InstanceController.MinecraftGamePath, instance.id),
@@ -47,17 +46,18 @@ export namespace LaunchController {
 			accessToken: (ApplicationStore.auth.store as Auth.Response).accessToken
 		};
 		// spawn game
-		// const proc = Launcher.launch(options);
+		const proc = launch(options);
+		return proc;
 
-		const minecraftFolder = MinecraftFolder.from(options.resourcePath as string);
-		await Launcher.ensureLibraries(minecraftFolder, options.version as ResolvedVersion);
-		await Launcher.ensureNative(minecraftFolder, options.version as ResolvedVersion, minecraftFolder.getNativesRoot((options.version as ResolvedVersion).id));
+		// const minecraftFolder = MinecraftFolder.from(options.resourcePath as string);
+		// await Launcher.ensureLibraries(minecraftFolder, options.version as ResolvedVersion);
+		// await Launcher.ensureNative(minecraftFolder, options.version as ResolvedVersion, minecraftFolder.getNativesRoot((options.version as ResolvedVersion).id));
 
-		const args: string[] = await Launcher.generateArguments(options); // get arguments from options
-		const spawnOptions = { cwd: options.gamePath, env: process.env, ...(options.extraExecOption || {}) };
-		console.log(`Launching instance ${name} with options: `, options, "and env: ", spawnOptions);
-		const spawn = child_process.spawn(args[0], args.slice(1), spawnOptions); // spawn java instance (args[0] should be "java" or java path from options.javaPath)
-		return spawn;
+		// const args: string[] = await Launcher.generateArguments(options); // get arguments from options
+		// const spawnOptions = { cwd: options.gamePath, env: process.env, ...(options.extraExecOption || {}) };
+		// console.log(`Launching instance ${name} with options: `, options, "and env: ", spawnOptions);
+		// const spawn = child_process.spawn(args[0], args.slice(1), spawnOptions); // spawn java instance (args[0] should be "java" or java path from options.javaPath)
+		// return spawn;
 	}
 
 }
