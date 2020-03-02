69c6917d2b49b7256ee4352ecde0d8f62f1d8f8a
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-01 18:57:04 -0800

    fix: implement custom version manifest fetch
    Built in functionality in @xmcl/installer is broken

--- src/renderer/controllers/VersionsController.ts
+++ src/renderer/controllers/VersionsController.ts
@@ -8,12 +8,14 @@ export namespace VersionsController {
 	 * @returns list of all availible vanilla versions or null if error
 	 */
 	export async function updateVersionMeta(): Promise<Installer.VersionList | null> {
-		const versionsMeta = await Installer.getVersionList();
+		// const versionsMeta = await Installer.getVersionList();
+		// FIXME: wait for fix
+		const versionsMeta = await (await fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json")).json() as Installer.VersionList;
 		if (!versionsMeta)
 			return null;
 		else {
 			// save to electron store
-			ApplicationStore.versionsMetaCache.set("versions", versionsMeta.versions);
+			ApplicationStore.versionsMetaCache.set(versionsMeta);
 			return versionsMeta;
 		}
 	}
