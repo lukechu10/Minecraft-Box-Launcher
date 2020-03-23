2a317906f93973616fbe581f67c61e2e482b3bb5
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-23 15:21:39 -0700

    fix: player name missing in UserStatus

--- src/renderer/components/UserStatus.ts
+++ src/renderer/components/UserStatus.ts
@@ -20,7 +20,7 @@ export default class UserStatus extends HTMLElement {
 		if (!authData.loggedIn)
 			this.innerHTML = userStatusTemplate({ loggedIn: false });
 		else
-			this.innerHTML = userStatusTemplate({ loggedIn: true, uuid: authData.selectedProfile.id });
+			this.innerHTML = userStatusTemplate({ loggedIn: true, ...authData.selectedProfile });
 		$(this).dropdown();
 	}
 }
