091b7c53d4473d0308333eb89e46b8bd4aa8a961
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-03-23 11:43:10 -0700

    Improve code coverage (#166)
    * chore: add chai
    
    * test: add tests for checking if new instance form allows empty form
    
    * test: increase timeout
    
    * test: add test for instance creation
    
    * chore: npm audit fix
    
    * test: remove .only()
    
    * test: add tests for instance modals
    
    * test: remove .only()
    
    * test: add test for instance rename modal
    
    * test: remove .only()
    
    * chore: npm audit fix
    
    * test: add test for instance confirm delete
    
    * test: rewrite assertions using chai
    
    * test: add test for instance options rename
    
    * chore(deps): update mocha
    
    * test: add test for instance installation
    
    * chore: skip install test for now
    
    * test: add tests for authentication modal
    
    * test: shoud not be able to create two instances with same name

--- test/spec.ts
+++ test/spec.ts
@@ -1,10 +1,10 @@
 "use-strict";
 import { Application } from "spectron";
-import assert from "assert";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import _ from "lodash";
 import fs from "fs-extra";
+import { expect } from "chai";
 
 const app = new Application({
 	path: require("electron") as unknown as string,
@@ -15,8 +15,8 @@ const app = new Application({
 	chromeDriverArgs: ["remote-debugging-port=9222"]
 });
 
-describe("Application launch", function () {
-	this.timeout(30000);
+describe("Application window", function () {
+	this.timeout(40000);
 
 	beforeEach(() => {
 		return app.start();
@@ -42,45 +42,247 @@ describe("Application launch", function () {
 
 	it("shows an initial window", async () => {
 		const n = await app.client.getWindowCount();
-		return assert.strictEqual(n, 1);
+		expect(n).to.be.a("number");
+		expect(n).to.equal(1);
 	});
 
 	it("has the correct title", async () => {
 		await app.client.waitUntilWindowLoaded();
 		const title = await app.browserWindow.getTitle();
-		return assert.strictEqual(title, "Minecraft Box");
+		expect(title).to.be.a("string");
+		expect(title).to.equal("Minecraft Box");
 	});
 
 	it("shows the instance page", async () => {
 		await app.client.waitUntilWindowLoaded();
 		const text = await app.client.getText("#content div h1");
-		return assert.strictEqual(text, "Instances");
+		expect(text).to.be.a("string");
+		expect(text).to.equal("Instances");
 	});
 
 	it("has no instances in the instance list", async () => {
 		await app.client.waitUntilWindowLoaded();
 		const list = await app.client.$$("div[is='instance-list'] .instance-item");
-		return assert.strictEqual(list.length, 0);
-	});
-
-	it("shows the new instance modal", async () => {
-		await app.client.waitUntilWindowLoaded();
-		await app.client.$("#content").$("div.ui.primary.button").click();
-		const res = await app.client.waitForVisible("#modal-newInstance", 1000);
-		return res;
+		expect(list).to.have.lengthOf(0);
 	});
 
 	it("shows the settings modal", async () => {
 		await app.client.waitUntilWindowLoaded();
 		await app.client.$("#content").$("div.ui.right.button").click();
-		const res = await app.client.waitForVisible("#modal-settings", 1000);
-		return res;
+		const res = await app.client.waitForVisible("#modal-settings:not(.animating)", 2000);
+		expect(res).to.equal(true);
 	});
 
-	it("shows the login modal", async () => {
-		await app.client.waitUntilWindowLoaded();
-		await app.client.$("#login-status-text").click();
-		const res = await app.client.waitForVisible("#modal-login");
-		return res;
+	describe("Authentication", () => {
+		describe("Authentication modal", () => {
+			it("shows the login modal", async () => {
+				await app.client.waitUntilWindowLoaded();
+				await app.client.$("#login-status-text").click();
+				const res = await app.client.waitForVisible("#modal-login:not(.animating)");
+				expect(res).to.equal(true);
+			});
+
+			it("should display error if no input", async () => {
+				await app.client.waitUntilWindowLoaded();
+				await app.client.$("#login-status-text").click();
+				const res = await app.client.waitForVisible("#modal-login:not(.animating)");
+				expect(res).to.equal(true);
+				await app.client.$("#login-btn").click(); // click on submit button
+				await app.client.waitForText("#login-errors-container", 1000);
+				expect(await app.client.$("#login-errors-container").getText()).to.equal("Please fill out the form!");
+			});
+
+			it.skip("should not submit if invalid email", async () => {
+				await app.client.waitUntilWindowLoaded();
+				await app.client.$("#login-status-text").click();
+				const res = await app.client.waitForVisible("#modal-login:not(.animating)");
+				expect(res).to.equal(true);
+				await app.client.$("#username-field").setValue("test@test.com"); // fill out username field
+				await app.client.$("#login-btn").click(); // click on submit button
+				await app.client.waitForText("#login-errors-container", 1000);
+				expect(await app.client.$("#login-errors-container").getText()).to.equal("Please fill out the form!");
+			});
+
+			it("should display invalid email / password error", async () => {
+				await app.client.waitUntilWindowLoaded();
+				await app.client.$("#login-status-text").click();
+				const res = await app.client.waitForVisible("#modal-login:not(.animating)");
+				expect(res).to.equal(true);
+				// fill out form
+				await app.client.$("#username-field").setValue("test@test.com");
+				await app.client.$("#password-field").setValue("test");
+				await app.client.$("#login-btn").click(); // click on submit button
+				// wait for error message to appear
+				await app.client.waitForText("#login-errors-container", 1000);
+				expect(await app.client.$("#login-errors-container").getText()).to.equal("Invalid username or password! Please try again.");
+			});
+		});
+	});
+
+	describe("Instance management", () => {
+		async function fillOutInstanceForm(name: string = "Test instance") {
+			await app.client.waitUntilWindowLoaded();
+			await app.client.$("#content").$("div.ui.primary.button").click();
+			const res = await app.client.waitForVisible("#modal-newInstance:not(.animating)", 2000);
+			expect(res).to.be.equal(true); // make sure modal appears
+			await app.client.$("#submit-newInstanceForm").click(); // attempt to create empty instance
+			const isError = await app.client.$("#modal-newInstance").waitForExist(".ui.form.error", 2000);
+			expect(isError).to.equal(true);
+
+			// fill out form
+			const form = app.client.$("#form-newInstance");
+			await form.$("input[name='instance-name']").setValue(name);
+			// select instance type
+			await form.$("#dropdown-type").click();
+			await form.$("#dropdown-type").waitForVisible(".menu:not(.animating)", 2000);
+			await form.$("#dropdown-type .menu .item").click(); // select first option (vanilla release)
+			await form.$("#dropdown-type").waitForVisible(".menu:not(.animating)", 2000, true);
+			// select instance version
+			await form.waitForExist("#dropdown-id:not(.disabled)");
+			await form.$("#dropdown-id").click();
+			await form.$("#dropdown-id").waitForVisible(".menu:not(.animating)", 2000);
+			await form.$("#dropdown-id .menu .item").click(); // select first option (latest vanilla release)
+			await form.$("#dropdown-id").waitForVisible(".menu:not(.animating)", 2000, true);
+
+			// click on create instance button
+			await form.$("#submit-newInstanceForm").click();
+
+			// check for no errors
+			const errorForm = await app.client.$("#modal-newInstance").$$(".ui.form.error");
+			expect(errorForm).to.have.lengthOf(0, "Error creating new instance");
+
+			// wait for modal to close
+			await app.client.$("#modal-newInstance").waitForVisible(2000, true);
+			// check if new item has been added to instance list
+			const instanceList = app.client.$("div[is='instance-list']");
+			const items = await instanceList.$$(".instance-item");
+			expect(items).to.have.lengthOf(1); // 1 instance
+			expect(await instanceList.$(".instance-item").$(".content .text-instanceName").getText()).to.equal(name); // check instance title (set in form)
+		}
+
+		async function openInstanceInfoModal() {
+			await fillOutInstanceForm();
+			await app.client.$("div[is='instance-list'").$(".instance-item .ui.grid .thirteen.wide.column").click();
+			await app.client.waitForVisible("#modal-info:not(.animating)", 2000);
+		}
+
+		it("can create new instances from the instance modal", async () => {
+			await fillOutInstanceForm();
+		});
+
+		it("can not create two instances with same name", async () => {
+			await fillOutInstanceForm("Test 1");
+			// fill out second form
+			await app.client.$("#content").$("div.ui.primary.button").click();
+			await app.client.waitForVisible("#modal-newInstance:not(.animating)", 2000);
+			// fill out form
+			const form = app.client.$("#form-newInstance");
+			await form.$("input[name='instance-name']").setValue("Test 1");
+			// select instance type
+			await form.$("#dropdown-type").click();
+			await form.$("#dropdown-type").waitForVisible(".menu:not(.animating)", 2000);
+			await form.$("#dropdown-type .menu .item").click(); // select first option (vanilla release)
+			await form.$("#dropdown-type").waitForVisible(".menu:not(.animating)", 2000, true);
+			// select instance version
+			await form.waitForExist("#dropdown-id:not(.disabled)");
+			await form.$("#dropdown-id").click();
+			await form.$("#dropdown-id").waitForVisible(".menu:not(.animating)", 2000);
+			await form.$("#dropdown-id .menu .item").click(); // select first option (latest vanilla release)
+			await form.$("#dropdown-id").waitForVisible(".menu:not(.animating)", 2000, true);
+			// click on create instance button
+			await form.$("#submit-newInstanceForm").click();
+			// check for no errors
+			const errorForm = await app.client.$("#modal-newInstance").$$(".ui.form.error");
+			expect(errorForm).to.have.lengthOf(1, "Can create multiple instances with same name");
+		});
+
+		it("can show the instance info modal", async () => {
+			await openInstanceInfoModal();
+		});
+
+		it("can show the instance rename modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-rename").click();
+			await app.client.waitForVisible("#modal-rename:not(.animating)", 2000);
+		});
+
+		it("can show the instance options modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-options").click();
+			await app.client.waitForVisible("#modal-options:not(.animating)", 2000);
+		});
+
+		it("can show the instance saves modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-saves").click();
+			await app.client.waitForVisible("#modal-saves:not(.animating)", 2000);
+		});
+
+		it("can show the instance confirm delete modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-delete").click();
+			await app.client.waitForVisible("#modal-confirmDelete:not(.animating)", 2000);
+		});
+
+		it("can delete an instance via instance confirm delete modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-delete").click();
+			await app.client.waitForVisible("#modal-confirmDelete:not(.animating)", 2000);
+			await app.client.$("#modal-confirmDelete").$(".ui.button.red").click();
+			await app.client.waitForVisible("#modal-confirmDelete:not(.animating)", 2000, true);
+			// make sure there are no more instances in instance list
+			const list = await app.client.$$("div[is='instance-list'] .instance-item");
+			expect(list).to.have.lengthOf(0);
+		});
+
+		it("can delete an instance via instance confirm delete modal without deleting folder", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-delete").click();
+			await app.client.waitForVisible("#modal-confirmDelete:not(.animating)", 2000);
+			await app.client.$("#modal-confirmDelete").$(".ui.button.red").click();
+			await app.client.$("#modal-confirmDelete").$("input[name='deleteFolder']").click();
+			await app.client.waitForVisible("#modal-confirmDelete:not(.animating)", 2000, true);
+			// make sure there are no more instances in instance list
+			const list = await app.client.$$("div[is='instance-list'] .instance-item");
+			expect(list).to.have.lengthOf(0);
+		});
+
+		it("can rename an instance with the rename modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-rename").click();
+			await app.client.waitForVisible("#modal-rename:not(.animating)", 2000);
+			await app.client.$("#input-rename").setValue("Test instance 2"); // rename modal
+			await app.client.$("#modal-rename").$(".ui.approve.button").click();
+			await app.client.waitForVisible("#modal-rename:not(.animating)", 2000, true);
+			// check that instance name in instance list changed
+			const instanceList = app.client.$("div[is='instance-list']");
+			expect(await instanceList.$(".instance-item").$(".content .text-instanceName").getText()).to.be.equal("Test instance 2");
+		});
+
+		it("can rename an instance with the options modal", async () => {
+			await openInstanceInfoModal();
+			await app.client.$(".btn-options").click();
+			await app.client.waitForVisible("#modal-options:not(.animating)", 2000);
+			await app.client.$("#modal-options").$("input[name='instance-name']").setValue("Test instance 2"); // options modal
+			await app.client.$("#modal-options").waitForExist("#btn-modalOptionsSave:not(.disabled)", 500); // wait for save button to become clickable
+			await app.client.$("div#btn-modalOptionsSave").click();
+			await app.client.waitForVisible("#modal-options:not(.animating)", 2000, true);
+			// check that instance name in instance list changed
+			const instanceList = app.client.$("div[is='instance-list']");
+			expect(await instanceList.$(".instance-item").$(".content .text-instanceName").getText()).to.be.equal("Test instance 2");
+		});
+
+		describe("Install and Launch", () => {
+			it.skip("can install latest release", async () => {
+				await fillOutInstanceForm();
+				const instanceList = app.client.$("div[is='instance-list']");
+				const instanceItem = instanceList.$(".instance-item");
+				await app.client.execute(() => {
+					(window as any).$(".instance-item")[0].install();
+				});
+				// wait for play button
+				await instanceItem.waitForExist(".btn-play", 20000); // 20 secs
+			});
+		});
 	});
 });
