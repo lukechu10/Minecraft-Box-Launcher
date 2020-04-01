8c7a66a564afc10f935506626ea49094922d00c4
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-04-01 12:32:36 -0700

    feat: rerender account modal on store change

--- src/renderer/components/AccountModal.ts
+++ src/renderer/components/AccountModal.ts
@@ -15,11 +15,24 @@ export default class AccountModal extends HTMLDivElement {
 			allowMultiple: false
 		}).modal("show");
 	}
+
+	public refresh(): void {
+		this.innerHTML = accountModalTemplate(AuthStore.store);
+	}
 }
 
 $(() => {
-	document.getElementById("account-modal-link")?.addEventListener("click", () => { 
+	document.getElementById("account-modal-link")?.addEventListener("click", () => {
 		(document.getElementById("modal-account") as AccountModal | null)?.render();
 	});
 });
+
+const changeCallback = () => {
+	console.log("Auth store changed, rendering user status");
+	document.querySelector<AccountModal>("#modal-account")?.refresh();
+};
+// @ts-ignore
+AuthStore.onDidChange("selectedProfile.name", changeCallback);
+AuthStore.onDidChange("loggedIn", changeCallback);
+
 customElements.define("modal-account", AccountModal, { extends: "div" });
