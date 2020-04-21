d68148efd986b56ac0cf75a3838bdc480d709583
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-04-21 15:27:16 -0700

    feat: set input error if instance exists already

--- src/renderer/components/InstanceModal/Rename.ts
+++ src/renderer/components/InstanceModal/Rename.ts
@@ -7,16 +7,20 @@ import { LitElement, customElement, property, html } from "lit-element";
 export default class Rename extends LitElement {
 	createRenderRoot(): this { return this; }
 
-	@property({ type: Object }) instance: Instance | null = null;
+	@property({ type: Object }) public instance: Instance | null = null;
+	@property({ type: Boolean }) private isError: boolean = false;
 
 	public render() {
 		return html`
 			<div class="header">Rename</div>
 			<div class="content">
-				<p>Enter a new name for instance ${this.instance?.name}</p>
-				<div class="ui fluid input">
+				<p>Enter a new name for instance <strong>${this.instance?.name}</strong></p>
+				<div class="ui fluid input ${this.isError ? "error" : ""}">
 					<input id="input-rename" type="text" name="newName" placeholder="New Name" value=${this.instance?.name ?? ""}>
 				</div>
+				${this.isError ? html`
+					<div class="ui pointing red basic label">An instance with that name already exists</div>
+				` : ""}
 			</div>
 			<div class="actions">
 				<button class="ui button cancel blue">Cancel</button>
@@ -26,6 +30,7 @@ export default class Rename extends LitElement {
 	}
 
 	public showModal(instance: Instance | null): void {
+		this.isError = false;
 		if (this.instance !== instance)
 			this.instance = instance; // set new active instance
 		else
@@ -35,10 +40,11 @@ export default class Rename extends LitElement {
 		$(this).modal({
 			closable: false,
 			onApprove: () => {
-				const newName = this.querySelector<HTMLInputElement>("#input-rename")?.value as string;
+				const input = this.querySelector<HTMLInputElement>("#input-rename");
+				const newName = input?.value as string;
 				const find = InstanceListStore.findInstanceName(newName); // make sure an instance with this name does not already exist
 				if (find !== null) {
-					alert("An instance with this name already exists"); // TODO: Change to modal to match rest of UI
+					this.isError = true;
 					return false;
 				}
 				else {
