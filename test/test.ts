185f5352e225c2913b51e9bdc8c9542c88122fc4
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-06-13 20:04:29 -0700

    test(codecov): setup code coverage for playwright test

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,9 @@
-import { electron, ElectronApplication, ElectronPage } from "playwright-electron";
-
-import path from "path";
 import { expect } from "chai";
+import fs from "fs-extra";
+import _ from "lodash";
+import path from "path";
+import { electron, ElectronApplication, ElectronPage } from "playwright-electron";
+import { v4 as uuidv4 } from "uuid";
 
 let electronApp: ElectronApplication = null;
 const electronPath = require("electron");
@@ -10,7 +12,7 @@ describe("Sanity checks", function () {
 	this.timeout(10000);
 
 	beforeEach(async function () {
-		// Before each test start Electron application.
+		// before each test start Electron application.
 		electronApp = await electron.launch(electronPath as unknown as string, {
 			// args: [path.join(__dirname, '..')]  // loads index.js
 			args: [".", "--dev"]
@@ -18,7 +20,21 @@ describe("Sanity checks", function () {
 	});
 
 	afterEach(async function () {
-		// After each test close Electron application.
+		// attempt to collect coverage object from global variable in renderer process
+		const coverageReport: object | undefined = await (await electronApp.firstWindow()).evaluate(() => (window as any).__coverage__);
+
+		// write coverage report to coverage dir
+		if (!_.isEmpty(coverageReport)) {
+			// generate file names
+			const NYC_OUTPUT_BASE = path.resolve(".nyc_output");
+			const NYC_OUTPUT_DEST = path.resolve(NYC_OUTPUT_BASE, `${uuidv4()}.json`);
+
+			await fs.mkdirp(NYC_OUTPUT_BASE); // make sure dir exists
+			await fs.writeJSON(NYC_OUTPUT_DEST, coverageReport, {
+				encoding: "utf8"
+			});
+		}
+		// after each test close Electron application.
 		await electronApp.close();
 	});
 
