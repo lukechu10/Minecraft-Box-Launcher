74d437555d5eef0fdfef73a16075b47dd8c53594
Luke Chu <lukechu10@gmail.com>
2020-06-15 16:47:57 +0000

    test: add more tests with playwright

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,8 @@
-import { expect } from "chai";
+import chai, { should } from "chai";
+should();
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
 import fs from "fs-extra";
 import _ from "lodash";
 import path from "path";
@@ -14,7 +18,7 @@ before(async () => {
 	// before each test start Electron application.
 	electronApp = await electron.launch(electronPath as unknown as string, {
 		// args: [path.join(__dirname, '..')]  // loads index.js
-		args: [".", "--dev"]
+		args: [".", "--dev", process.env.GITPOD_HOST === "https://gitpod.io" ? "--no-sandbox" : ""] // run without sandboxing if using gitpod]
 	});
 
 	page = await electronApp.firstWindow(); // wait for window to load
@@ -44,18 +48,31 @@ describe("Window", function () {
 
 	it("has the correct window title", async () => {
 		const title: string = await page.mainFrame().title();
-		expect(title).to.equal("Minecraft Box");
+		title.should.equal("Minecraft Box");
 	});
 
 	it("starts on the instance list page", async () => {
-		const header = await page.$(".ui.container > div > h1");
-		expect(await header.textContent()).to.equal("Instances");
+		const header = await page.$("#content > div > h1");
+		header.textContent().should.eventually.equal("Instances");
 	});
 
 	it("can navigate to home page", async () => {
-		await (await page.$("#app-navbar > .item[href='./home.html']")).click();
+		await page.click("#app-navbar > .item[href='./home.html']");
 
-		const header = await page.$(".ui.container > div > h1");
-		expect(await header.textContent()).to.equal("Home");
+		const header = await page.waitForSelector("#content > div > h1 >> text='Home'", { timeout: 1000 });
+		header.textContent().should.eventually.equal("Home");
+	});
+
+	it("can navigate to news page", async () => {
+		await page.click("#app-navbar > .item[href='./news.html']");
+
+		const header = await page.waitForSelector("#content > div > h1 >> text='News'", { timeout: 1000 });
+		header.textContent().should.eventually.equal("News");
+	});
+
+	it("can open account modal", async () => {
+		await page.click("#app-navbar > #account-modal-link");
+
+		await page.waitForSelector("#modal-account.ui.modal.visible", { timeout: 2000 });
 	});
 });
