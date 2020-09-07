805392e38ef131279e83acaa3a176d0e9ab007af
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-09-07 14:48:20 -0700

    feat: logs

--- src/renderer/components/instance/LogsPage.ts
+++ src/renderer/components/instance/LogsPage.ts
@@ -9,9 +9,22 @@ export class LogsPage extends LitElement {
 
 	protected render(): TemplateResult {
 		if (this.instance !== null) {
+			const msgTemplates: TemplateResult[] = [];
+			if (this.instance.process?.log !== undefined) {
+				for (const msg of this.instance.process?.log) {
+					msgTemplates.push(html`
+						<div class="InstanceLogs-out ${msg.type === "err" ? "InstanceLogs-error" : ""}">${msg.message}</div>
+					`);
+				}
+			}
+
 			return html`
 				<div class="ui header">Logs</div>
-				<p>Coming soon</p>
+				${msgTemplates.length > 0 ? html`
+					${msgTemplates}
+				` : html`
+					<p>Launch the instance to see the game log.</p>
+				`}
 			`;
 		}
 		else {
